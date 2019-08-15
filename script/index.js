b9e127182893d6a206512c485b7041eee8365a9b
Gerald Michael Austria <devrald24@gmail.com>
2019-08-15 23:59:54 +0800

    updated lazy load demo

--- script/index.js
+++ script/index.js
@@ -1,9 +1,6 @@
 if ('loading' in HTMLImageElement.prototype) {
-	const images = document.querySelectorAll("img.lazyload");
-	images.forEach(img => {
-		img.src = img.dataset.src;
-	});
-	} else {
+	
+} else {
 	// Dynamically import the LazySizes library
 	let script = document.createElement("script");
 	script.async = true;
